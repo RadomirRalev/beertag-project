3be245502147528a8c511d39edbb65f6f97cebbe
Radomir Ralev <rado_ralev@yahoo.com>
2019-12-02 18:03:31 +0200

    "Adds basic Spring configuration for the project."

---
+++ Solution/src/test/java/com/beertag/demo/BeertagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beertag.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeertagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
