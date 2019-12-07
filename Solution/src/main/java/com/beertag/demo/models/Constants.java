db7625244d54bcd5564dcb44a56537ec85538508
VasilDinchev <vasildinchev@gmail.com>
2019-12-08 01:07:35 +0200

    fix Tag and User CRUD return type

---
+++ Solution/src/main/java/com/beertag/demo/models/Constants.java
@@ -0,0 +1,26 @@
+package com.beertag.demo.models;
+
+public class Constants {
+    public static final String LIST_EMPTY = "List is empty.";
+    public static final String BEER_NAME_NOT_FOUND = "Beer %s not found in the database.";
+    public static final String BEER_ID_NOT_FOUND = "Beer with %d not found in the database.";
+    public static final String BEER_NAME_EXISTS = "Beer %s already exists.";
+    public static final String USER_ALREADY_EXISTS = "User already exists.";
+    public static final String EMAIL_ALREADY_EXISTS = "Email already exists.";
+    public static final String USER_NOT_FOUND = "User with name %s not found.";
+    public static final String BREWERY_NAME_NOT_FOUND = "Brewery %s not found in the database.";
+    public static final String BREWERY_ID_NOT_FOUND = "Brewery with %d not found in the database.";
+    public static final String BREWERY_NAME_EXISTS = "Brewery %s already exists.";
+    public static final String COUNTRY_NAME_NOT_FOUND = "Country %s not found in the database.";
+    public static final String COUNTRY_ID_NOT_FOUND = "Country with %d not found in the database.";
+    public static final String COUNTRY_NAME_EXISTS = "Country %s already exists.";
+    public static final String STYLE_NAME_NOT_FOUND = "Style %s not found in the database.";
+    public static final String STYLE_ID_NOT_FOUND = "Style with %d not found in the database.";
+    public static final String STYLE_NAME_EXISTS = "Style %s already exists.";
+    public static final String TAG_NAME_NOT_FOUND = "Tag %s not found in the database.";
+    public static final String TAG_ID_NOT_FOUND = "Tag with %d not found in the database.";
+    public static final String TAG_NAME_EXISTS = "Tag %s already exists.";
+    public static final String USER_NAME_NOT_FOUND = "User %s not found in the database.";
+    public static final String USER_ID_NOT_FOUND = "User with %d not found in the database.";
+    public static final String USER_NAME_EXISTS = "User %s already exists.";
+}
---
+++ Solution/src/main/java/com/beertag/demo/models/beer/Style.java
@@ -0,0 +1,30 @@
+package com.beertag.demo.models.beer;
+
+public class Style {
+    private int id;
+    private String name;
+
+    public Style() {
+
+    }
+
+    public Style(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
