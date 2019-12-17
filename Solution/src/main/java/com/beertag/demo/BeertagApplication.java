fa1e50421c0d303bf3afa5d8dd2f6d0b15ace44d
Radomir Ralev <rado_ralev@yahoo.com>
2019-12-18 00:38:54 +0200

    "Creates HibernateConfig file, configures Hibernate."

--- Solution/src/main/java/com/beertag/demo/BeertagApplication.java
+++ Solution/src/main/java/com/beertag/demo/BeertagApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BeertagApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BeertagApplication.class, args);
     }
 
